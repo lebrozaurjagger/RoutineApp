5331cdfc07ea65fcdcdd57d69d780c057f2caa37
Phillip <78912067+lebrozaurjagger@users.noreply.github.com>
2024-02-14 18:54:35 +0300

    0.1.1
    Created project files

---
+++ RoutineApp/RoutineApp/RoutineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineAppApp.swift
+//  RoutineApp
+//
+//  Created by Phillip on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct RoutineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
